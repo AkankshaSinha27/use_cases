c25fa6c366e410d97667481a4561c86628624b53
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-10-20 16:05:21 +0530

    commit for assignment20

---
+++ Assign_Insurance/src/main/java/com/Insurance/Assign_Insurance/AssignInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.Insurance.Assign_Insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignInsuranceApplication.class, args);
+	}
+
+}
---
+++ Assign_Insurance/src/test/java/com/Insurance/Assign_Insurance/AssignInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Insurance.Assign_Insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
