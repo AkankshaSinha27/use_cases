e2ac8b7c2d37c462cba88e311921fbb8783e6835
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-10-27 15:52:45 +0530

    Commit 26date

---
+++ Assign_Insurance/src/main/java/com/Insurance/Assign_Insurance/Equal.java
@@ -0,0 +1,19 @@
+package com.Insurance.Assign_Insurance;
+
+public class Equal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a="Hi";
+		String b="Hi";
+		String c= new String ("Hi");
+		
+		System.out.println(a==b);
+		System.out.println(a==c);
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
+		
+	}
+
+}
--- Assign_Insurance/src/main/java/com/Insurance/Assign_Insurance/InsurController.java
+++ Assign_Insurance/src/main/java/com/Insurance/Assign_Insurance/InsurController.java
@@ -4,27 +4,48 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-
-
 
 @RestController
 public class InsurController {
 
 	@Autowired
 	private InsurRepo ir;
+
 	@GetMapping("/c1")
 	public List<InsurEntity> joinco() {
 
 		return ir.getjoin(1);
 	}
-	
-	
+
 	@GetMapping("/c2")
 	public List<InsurEntity> joinco1() {
 		return ir.getjoin(2);
 	}
-	
-	
+
+	@GetMapping("/client_discount/{tenure}/{premium}")
+	public int discount(@PathVariable String tenure, @PathVariable int premium) {
+		if (tenure.equals("morethan5")) {
+			premium = (int) (premium - (premium * 0.12));
+			return premium;
+		}
+
+		if (tenure.equals("equalto5")) {
+
+			premium = (int) (premium - (premium * 0.1));
+			return premium;
+		}
+
+		if (tenure.equals("lessthan5")) {
+
+			return premium;
+		}
+
+		else {
+			return 0;
+		}
+
+	}
+
 }
