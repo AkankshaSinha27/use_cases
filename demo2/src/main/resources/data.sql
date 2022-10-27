3cb0a02bd5e4af9d899d3eede4327f29656f255c
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-10-27 23:28:46 +0530

    commit H2 DB

---
+++ demo2/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10001,'USD', 'INR' ,65,0);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10002,'EUR', 'INR' ,75,0);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10003,'AUD', 'INR' ,25,0);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10004,'YEN', 'INR' ,200,0); 
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10005,'FIR', 'INR' ,70,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10006,'HI', 'INR' ,56,0);
